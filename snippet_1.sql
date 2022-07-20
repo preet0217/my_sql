4e5e1eb3603bf9e1cf60c77ae025a01aa2c8fea8
Ranjeetha Virdi <81987445+preet0217@users.noreply.github.com>
2022-07-20 11:56:20 +0200

    Delete snippet_1.sql

--- snippet_1.sql
+++
@@ -1,9 +0,0 @@
-%run ../Includes/Classroom-Setup
-
-%fs head /mnt/davis/fire-incidents/fire-incidents-2016.csv %run ../Includes/Classroom-Setup
-
-
-CREATE DATABASE IF NOT EXISTS Databricks
-USE Databricks
-
-DROP TABLE IF EXISTS fireincidents;
